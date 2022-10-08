4fdcf473da59d44392dc1542f775ce5cc81f888e
Jorman Copete <copesan@hotmail.com>
2022-10-08 05:11:49 -0500

    Configuración final del proyecto

--- backend/src/backend.py
+++ backend/src/backend.py
@@ -4,6 +4,8 @@ from turtle import width
 
 import grpc
 import cv2
+import numpy as np
+import tensorflow as tf
 
 import backend_pb2
 import backend_pb2_grpc
@@ -31,6 +33,47 @@ class BackendService(backend_pb2_grpc.BackendServicer):
         response_message = backend_pb2.image(img_content=image_str, width=w, height=h)
         return response_message
 
+    def read_jpg_file(self, path):
+        img = cv2.imread(path)
+        img_array = np.asarray(img)
+        #img2show = Image.fromarray(img_array)
+        img2 = img_array.astype(float)
+        img2 = (np.maximum(img2, 0) / img2.max()) * 255.0
+        img2 = np.uint8(img2)
+        return img2
+
+    def predict(self, request, context):
+        # load the image from disk
+        path = request.path
+        array = self.read_jpg_file(path)
+        #   1. call function to pre-process image: it returns image in batch format
+        batch_array_img = self.preprocess(array)
+        #   2. call function to load model and predict: it returns predicted class and probability
+        model = tf.keras.models.load_model('WilhemNet_86.h5')
+
+        prediction = np.argmax(model.predict(batch_array_img))
+        #proba = np.max(model.predict(batch_array_img)) * 100
+        label = ""
+        if prediction == 0:
+            label = "bacteriana"
+        if prediction == 1:
+            label = "normal"
+        if prediction == 2:
+            label = "viral"
+        #   3. call function to generate Grad-CAM: it returns an image with a superimposed heatmap
+        response_message = backend_pb2.prediction_result(label_prediction=label)
+        return response_message
+
+
+    def preprocess(self, array):
+        array = cv2.resize(array, (512, 512))
+        array = cv2.cvtColor(array, cv2.COLOR_BGR2GRAY)
+        clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(4, 4))
+        array = clahe.apply(array)
+        array = array / 255
+        array = np.expand_dims(array, axis=-1)
+        array = np.expand_dims(array, axis=0)
+        return array              
 
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
--- interface/src/interface.py
+++ interface/src/interface.py
@@ -1,5 +1,6 @@
 # import the necessary packages
-from tkinter import Tk, Button, Label, filedialog
+from tkinter import *
+from tkinter import Tk, Button, Label, filedialog, ttk
 import base64
 
 import grpc
@@ -10,17 +11,19 @@ import numpy as np
 import backend_pb2
 import backend_pb2_grpc
 
+path_img_global = ""
 
 def select_image():
     # grab a reference to the image panels
-    global panelA, backend_client
+    global panelA, backend_client, path_img_global
     # open a file chooser dialog and allow the user to select an input
     # image
     path = filedialog.askopenfilename()
 
     # ensure a file path was selected
     if len(path) > 0:
-
+        path_img_global = path
+        print("path:" + path + " <> path_img_global:" + path_img_global)
         path_message = backend_pb2.img_path(path=path)
         response = backend_client.load_image(path_message)
 
@@ -47,19 +50,41 @@ def select_image():
             panelA.configure(image=image)
             panelA.image = image
 
+def select_model():
+     # grab a reference to the image panels
+    global panelA, backend_client, path_img_global
+    response = None
+    # ensure a file path was selected
+    if len(path_img_global) > 0:
+        print(path_img_global)
+        path_message = backend_pb2.img_predic_ruta(path_img_predic_ruta=path_img_global)
+        response = backend_client.predict(path_message)
+        text1.insert(END, response.label_prediction)
 
 # initialize the window toolkit along with the two image panels
 root = Tk()
 panelA = None
 
 # Backend client definition
-channel = grpc.insecure_channel("backend:50051")
+options = [('grpc.max_send_message_length', 256*1024*1024),('grpc.max_receive_message_length', 256 * 1024 * 1024)]
+channel = grpc.insecure_channel("backend:50051", options=options)
 backend_client = backend_pb2_grpc.BackendStub(channel=channel)
 
 # create a button, then when pressed, will trigger a file chooser
 # dialog and allow the user to select an input image; then add the
-# button the GUI
+# button the GUI´
+ID = StringVar()
+result = StringVar()
+text1 = ttk.Entry(root, textvariable=ID, width=10)
+
+lblPredic = Label(root,text="Resultado prediccion:")
+lblPredic.pack(side="bottom",fill="both")
+
+btnPredic = Button(root, text="Predecir", command=select_model)
+btnPredic.pack(side="bottom", fill="both", expand="yes", padx="10", pady="10")
+
 btn = Button(root, text="Select an image", command=select_image)
 btn.pack(side="bottom", fill="both", expand="yes", padx="10", pady="10")
+
 # kick off the GUI
 root.mainloop()
